098e3ac42de80124d13c4f751700d185a6bb2f6b
saurabh <saurabhkatwal@gmail.com>
2022-09-29 21:48:35 +0530

    added styling to message section

--- src/components/MessageInfo.js
+++ src/components/MessageInfo.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react'
-
+import "./css/message.css"
 export default class MessageInfo extends Component {
   render() {
     return (
       <div className='message-info-section'>
-        <div className="message">
+        <div className="message inputDiv">
             <label htmlFor="">Message</label>
             <textarea name="" id="" cols="30" rows="10"></textarea>
         </div>
         <div className="choices">
-            <div className="choice1">
-            <input type="radio" name="choice" id="" />
-            <label htmlFor="">The number 1 choice</label>
+            <div className="choice1 choice">
+            <input type="radio" name="choice" id="choiceOne" />
+            <label htmlFor="choiceOne">The number 1 choice</label>
             </div>
-            <div className="choice2">
-                <input type="radio" name="choice" id="" />
-                <label htmlFor="">The number 2 choice</label>
+            <div className="choice2 choice">
+                <input type="radio" name="choice" id="choiceTwo" />
+                <label htmlFor="choiceTwo">The number 2 choice</label>
             </div>
         </div>
       </div>
