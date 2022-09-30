0a4f16abe827f890de40e92da14df5355c28230d
saurabh <saurabhkatwal@gmail.com>
2022-10-01 01:41:12 +0530

    increased size of form box

--- src/components/SignUpInfo.js
+++ src/components/SignUpInfo.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import "./css/signup.css"
 import Buttons from './Buttons'
 export default class SignUpInfo extends Component {
+  
   render() {
     return (
       <div className="sign-up-section">
