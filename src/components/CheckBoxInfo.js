de107dc6ac64eb4eaf4a893e8832429874fa84e6
saurabh <saurabhkatwal@gmail.com>
2022-09-30 09:59:58 +0530

    added checkbox content and styling to the checkbox component

--- src/components/CheckBoxInfo.js
+++ src/components/CheckBoxInfo.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react'
-
+import "./css/checkbox.css"
 export default class CheckBoxInfo extends Component {
   render() {
     return (
       <div className='checkbox-info-section'>
         <div className="icons">
             <div className="icon1">
-            <i className="fa-solid fa-dice-one fa-3x"></i>
+            <i className="fa-solid fa-dice-one fa-8x"></i>
             </div>
             <div className="icon2">
-            <i className="fa-solid fa-dice-two fa-3x"></i>
+            <i className="fa-solid fa-dice-two fa-8x"></i>
             </div>
+        </div>
+        <div className="checkboxData">
+          <div className="check1 check">
+          <input type="checkbox" name="" id="" />
+          <label htmlFor="">I want to add this option</label>
+          </div>
+          <div className="check2 check">
+            <input type="checkbox" name="" id="" />
+            <label htmlFor="">Let me click on this checkbox and choose some cool stuff</label>
+          </div>
         </div>
       </div>
     )
